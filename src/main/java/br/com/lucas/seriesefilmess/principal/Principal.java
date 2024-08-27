68679588edef419de8d17cf0367ac9adc5edc901
Lucas Cunha <lucas71cunha@gmail.com>
2024-08-27 01:00:01 -0300

    Updating repository

--- src/main/java/br/com/lucas/seriesefilmess/principal/Principal.java
+++ src/main/java/br/com/lucas/seriesefilmess/principal/Principal.java
@@ -1,9 +1,6 @@
 package br.com.lucas.seriesefilmess.principal;
 
-import br.com.lucas.seriesefilmess.model.DadosSerie;
-import br.com.lucas.seriesefilmess.model.DadosTemporada;
-import br.com.lucas.seriesefilmess.model.Episodio;
-import br.com.lucas.seriesefilmess.model.Serie;
+import br.com.lucas.seriesefilmess.model.*;
 import br.com.lucas.seriesefilmess.repository.SerieRepository;
 import br.com.lucas.seriesefilmess.service.ConsumoApi;
 import br.com.lucas.seriesefilmess.service.ConverteDados;
@@ -39,6 +36,9 @@ public class Principal {
                     2 - Buscar episódios
                     3 - Listar séries buscadas
                     4 - Buscar série por titulo
+                    5 - Buscar séries por ator
+                    6 - Top 5 séries
+                    7 - Buscar séries por categoria
                     
                     0 - Sair
                     """;
@@ -59,6 +59,15 @@ public class Principal {
                     break;
                 case 4:
                     buscarSeriePorTitulo();
+                    break;
+                case 5:
+                    buscarSeriePorAtor();
+                    break;
+                case 6:
+                    buscarTop5Series();
+                    break;
+                case 7:
+                    buscarSeriesPorCategoria();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -95,9 +104,7 @@ public class Principal {
         System.out.println("Escolha uma série pelo nome: ");
         var nomeSerie = leitura.nextLine();
 
-        Optional<Serie> serie = series.stream()
-                .filter(s -> s.getTitulo().toLowerCase().contains(nomeSerie.toLowerCase()))
-                .findFirst();
+        Optional<Serie> serie = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
 
         if (serie.isPresent()) {
 
@@ -140,5 +147,30 @@ public class Principal {
         } else {
             System.out.println("Série não encontrada!");
         }
+    }
+    private void buscarSeriePorAtor() {
+        System.out.println("Escolha uma série pelo ator: ");
+        var nomeAtor = leitura.nextLine();
+        System.out.println("Avaliacões a partir de que valor? ");
+        var avaliacao = leitura.nextDouble();
+        List<Serie> serieEncontradas = repositorio.findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(nomeAtor, avaliacao);
+        System.out.println("Séries em que " + nomeAtor + " trabalhou: ");
+        serieEncontradas.forEach(s ->
+                System.out.println(s.getTitulo() + " avaliação: " + s.getAvaliacao()));
+    }
+
+    private void buscarTop5Series() {
+        List<Serie> serieTop = repositorio.findTop5ByOrderByAvaliacaoDesc();
+        serieTop.forEach(s ->
+                System.out.println(s.getTitulo() + " avaliação: " + s.getAvaliacao()));
+    }
+
+    private void buscarSeriesPorCategoria() {
+        System.out.println("Deseja buscar séries de que categoria/gênero? ");
+        var nomeGenero = leitura.nextLine();
+        Categoria categoria = Categoria.fromPortugues(nomeGenero);
+        List<Serie> seriesPorCategoria = repositorio.findByGenero(categoria);
+        System.out.println("Séries da categoria " + nomeGenero);
+        seriesPorCategoria.forEach(System.out::println);
     }
 }
