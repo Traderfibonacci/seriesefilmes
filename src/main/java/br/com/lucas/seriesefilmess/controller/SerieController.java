243babd88eee75648345bf314d4e098f81cd3d63
Lucas Cunha <lucas71cunha@gmail.com>
2024-09-05 15:11:19 -0300

    Updating service

--- src/main/java/br/com/lucas/seriesefilmess/controller/SerieController.java
+++ src/main/java/br/com/lucas/seriesefilmess/controller/SerieController.java
@@ -1,5 +1,6 @@
 package br.com.lucas.seriesefilmess.controller;
 
+import br.com.lucas.seriesefilmess.dto.EpisodioDTO;
 import br.com.lucas.seriesefilmess.dto.SerieDTO;
 import br.com.lucas.seriesefilmess.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,5 +37,19 @@ public class SerieController {
     @GetMapping("/{id}")
     public SerieDTO obterPorId(@PathVariable Long id) {
         return servico.obterPorId(id);
+    }
+
+    @GetMapping("/{id}/temporadas/todas")
+    public List<EpisodioDTO> obterTodasTemporadas(@PathVariable Long id){
+        return servico.obterTodasTemporadas(id);
+    }
+
+    @GetMapping("/{id}/temporadas/{numero}")
+    public List<EpisodioDTO> obterTemporadasPorNumero(@PathVariable Long id, @PathVariable Long numero){
+        return servico.obterTemporadasPorNumero(id, numero);
+    }
+    @GetMapping("/categoria/{nomeGenero}")
+    public List<SerieDTO> obterSeriesPorCategoria(@PathVariable String nomeGenero){
+        return servico.obterSeriesPorCategoria(nomeGenero);
     }
 }
---
+++ src/main/java/br/com/lucas/seriesefilmess/dto/EpisodioDTO.java
@@ -0,0 +1,4 @@
+package br.com.lucas.seriesefilmess.dto;
+
+public record EpisodioDTO(Integer temporada, Integer numeroEpisodio, String titulo) {
+}
